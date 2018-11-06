e5fc49765ed5b4f5ec764a67373dc53a7c2bc926
CuongLH <alzncu95@gmail.com>
2018-11-06 14:46:05 +0700

    add detect currency

---
+++ currency_rate_crawl/currency_id.py
@@ -0,0 +1,19 @@
+def detectCurrencyId(str_currency):
+
+    if (str_currency == 'JPY'): return 1
+
+    if (str_currency == 'USD'): return 2
+
+    if (str_currency == 'IDR'): return 3
+
+    if (str_currency == 'THB'): return 4
+
+    if (str_currency == 'VND'): return 5
+
+    if (str_currency == 'SGD'): return 6
+
+    if (str_currency == 'PHP'): return 7
+
+    if (str_currency == 'RM'): return 8
+
+    return 0
