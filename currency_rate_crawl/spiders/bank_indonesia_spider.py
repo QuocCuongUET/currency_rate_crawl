cd64fb3a5faffc2154603fc6c3e0a4e5ad0748f6
CuongLH <alzncu95@gmail.com>
2018-11-06 13:40:36 +0700

    format  code

--- currency_rate_crawl/spiders/bank_indonesia_spider.py
+++ currency_rate_crawl/spiders/bank_indonesia_spider.py
@@ -7,11 +7,11 @@ class BankIndonesiaSpider(scrapy.Spider):
 		url = 'https://www.bi.go.id/en/moneter/informasi-kurs/transaksi-bi/Default.aspx'
 		yield scrapy.Request(url, self.parse)
 
-	def parse(self, response):
+       def parse(self, response):
 
 		# Get date of crawl
 		date = response.css('span#ctl00_PlaceHolderMain_biWebKursTransaksiBI_lblUpdate::text').extract_first()
-		
+
 		# Get table contain data
 		table = response.css('table#ctl00_PlaceHolderMain_biWebKursTransaksiBI_GridView2')
 
@@ -20,14 +20,11 @@ class BankIndonesiaSpider(scrapy.Spider):
 			td = tr.css('td::text')
 
 			if td:
-		
 				data = {
-					'Currencies' : td[0].extract(),
-					'Value'      : td[1].extract(),
-					'Sell'		 : td[2].extract(),
-					'Buy'		 : td[3].extract(),
-				}
+                    'Currencies' : td[0].extract(),
+                    'Value'      : td[1].extract(),
+                    'Sell'		 : td[2].extract(),
+                    'Buy'		 : td[3].extract(),
+                }
 
-				yield data
-
-
+               yield data
